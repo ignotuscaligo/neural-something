c021c83ba90fe3b608ae723504347313b676d25d
Elijah Kleeman <contact@elijahk.com>
2017-10-12 00:05:43 -0700

    Moved files over from working project

---
+++ perceptron.py
@@ -0,0 +1,99 @@
+from math import exp
+from random import random
+
+def sigmoid(x):
+    return 1.0 / (1.0 + exp(-x))
+
+def derivative_sigmoid(x):
+    return x * (1.0 - x)
+
+class Connection:
+    def __init__(self, source=None):
+        self.weight = 1.0
+        self.source = source
+
+    def get_value(self):
+        return self.source.value * self.weight
+
+class Perceptron:
+    def __init__(self):
+        self.index = -1
+        self.connections = []
+        self.bias = 0.0
+        self.value = 0.0
+        self.slope = 0.0
+        self.delta = 0.0
+
+    def forward(self):
+        self.value = self.bias
+
+        for connection in self.connections:
+            self.value += connection.get_value()
+
+        self.value = sigmoid(self.value)
+
+    def backward_propagate_error(self, error):
+        self.slope = derivative_sigmoid(self.value)
+        self.delta = self.slope * error
+
+        for connection in self.connections:
+            connection.source.backward_propagate_error(self.delta * connection.weight)
+
+    def update_weights(self, delta_sum, learning_rate):
+        for connection in self.connections:
+            connection.weight += self.delta * connection.source.value * learning_rate
+
+        self.bias += delta_sum * learning_rate
+
+    def add_connection_from(self, other):
+        self.connections.append(Connection(source=other))
+
+    def randomize_weights(self):
+        self.bias = random()
+        for connection in self.connections:
+            connection.weight = random()
+
+class Layer:
+    def __init__(self):
+        self.index = -1
+        self.nodes = []
+
+    def add_node(self):
+        new_node = Perceptron()
+        new_node.index = len(self.nodes)
+        self.nodes.append(new_node)
+
+    def add_nodes(self, count):
+        for _ in range(count):
+            self.add_node()
+
+    def connect_nodes_from_layer(self, layer):
+        for node in self.nodes:
+            for other_node in layer.nodes:
+                print("Connecting node {0}:{1} to {2}:{3}".format(
+                        layer.index, other_node.index,
+                        self.index, node.index))
+
+                node.add_connection_from(other_node)
+
+    def forward(self):
+        for node in self.nodes:
+            node.forward()
+
+    def backward_propagate_error(self, error):
+        for node in self.nodes:
+            node.backward_propagate_error(error)
+
+    def update_weights(self, learning_rate):
+        delta_sum = 0
+
+        for node in self.nodes:
+            delta_sum += node.delta
+
+        for node in self.nodes:
+            node.update_weights(delta_sum, learning_rate)
+
+    def randomize_weights(self):
+        for node in self.nodes:
+            node.randomize_weights()
+
---
+++ perceptron_test.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+from argparse import ArgumentParser
+from random import random
+
+from perceptron import *
+
+if __name__ == "__main__":
+    print("Hello!")
+
+    parser = ArgumentParser()
+    parser.add_argument("epoch_count", type=int, help="Number of training epochs")
+    parser.add_argument("learning_rate", type=float, help="Learning rate")
+    args = parser.parse_args()
+
+    epoch_count = args.epoch_count
+    learning_rate = args.learning_rate
+
+    print("Building network")
+    input_count = 3
+    hidden_count = 5
+    output_count = 1
+
+    input_layer = Layer()
+    hidden_layer = Layer()
+    output_layer = Layer()
+    
+    input_layer.index = 0
+    hidden_layer.index = 1
+    output_layer.index = 2
+
+    input_layer.add_nodes(input_count)
+    hidden_layer.add_nodes(hidden_count)
+    output_layer.add_nodes(output_count)
+
+    hidden_layer.connect_nodes_from_layer(input_layer)
+    output_layer.connect_nodes_from_layer(hidden_layer)
+
+    input_layer.nodes[0].value = 0.5
+    input_layer.nodes[1].value = 0.5
+    input_layer.nodes[2].value = 0.5
+
+    hidden_layer.randomize_weights()
+    output_layer.randomize_weights()
+
+    print("Training network: {0} epochs, {1} learning rate".format(
+            epoch_count, learning_rate))
+
+    for epoch in range(epoch_count):
+        #print("Running epoch #{0}".format(epoch + 1))
+
+        inputs = [random(), random(), random()]
+        expected_output = (1.0 - inputs[0]) * (inputs[1]) * (1.0 - inputs[2])
+
+        #print("Input / Expected Output: {0} / {1}".format(inputs, expected_output))
+
+        input_layer.nodes[0].value = inputs[0]
+        input_layer.nodes[1].value = inputs[1]
+        input_layer.nodes[2].value = inputs[2]
+
+        hidden_layer.forward()
+        output_layer.forward()
+
+        predicted_output = output_layer.nodes[0].value
+        output_error = expected_output - predicted_output
+
+        #print("Predicted output: {0} ({1})".format(
+        #        predicted_output, output_error))
+
+        # steps through entire network
+        output_layer.backward_propagate_error(output_error)
+
+        #print("Training")
+        output_layer.update_weights(learning_rate)
+        hidden_layer.update_weights(learning_rate)
+
+    
+    test_count = 100
+    average_error = 0
+
+    print("Testing network")
+    for test in range(test_count):
+        #print("Running test #{0}".format(test + 1))
+
+        inputs = [random(), random(), random()]
+        expected_output = (1.0 - inputs[0]) * (inputs[1]) * (1.0 - inputs[2])
+
+        #print("Input / Expected Output: {0} / {1}".format(inputs, expected_output))
+
+        input_layer.nodes[0].value = inputs[0]
+        input_layer.nodes[1].value = inputs[1]
+        input_layer.nodes[2].value = inputs[2]
+
+        hidden_layer.forward()
+        output_layer.forward()
+
+        predicted_output = output_layer.nodes[0].value
+        output_error = expected_output - predicted_output
+
+        print("Predicted / Expected output: {0} / {1} ({2})".format(
+                predicted_output, expected_output, output_error))
+
+        average_error += output_error
+
+    average_error /= (test_count * 1.0)
+    print("Average error on test: {0}".format(average_error))
+
+    print("Goodbye!")
+
